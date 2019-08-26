d3f694fc7a6861b72bf49bf95c2cf961b5caa8ba
ahmadfirdaus06 <ahmad0609firdaus@gmail.com>
2019-08-27 01:46:11 +0800

    configure image upload api

--- semms/api/report/upload-images.php
+++ semms/api/report/upload-images.php
@@ -1,19 +1,16 @@
 <?php
 
 date_default_timezone_set("Asia/Kuala_Lumpur");
-$target_dir = "uploads/";
-
-$file_name = "SEMMS_" . strtoupper(date("Ymd_His")) . ".jpeg";
+$target_dir = "../../../../semms-uploads/";
+$temp_file = basename($_FILES["file"]["name"]);
+$file_type = pathinfo($temp_file,PATHINFO_EXTENSION);
+$uniquesavename = time().uniqid(rand());
+$file_name = "SEMMS_" . strtoupper(date("Ymd_His_") . $uniquesavename)  . "." .$file_type;
 $target_file = $target_dir . $file_name;
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)){
 	echo json_encode(
             array('image_path' => $target_file), JSON_UNESCAPED_SLASHES
         );
-}
-else{
-    echo json_encode(
-        array('image_path' => "")
-    );
 }
 
 ?>
