67b4291baa8bd412c0ce27c22738c32a5d059de3
ahmadfirdaus06 <ahmad0609firdaus@gmail.com>
2019-08-27 03:54:27 +0800

    configure upload api

--- semms/api/report/upload-report-details.php
+++ semms/api/report/upload-report-details.php
@@ -14,23 +14,25 @@
 
     $data = json_decode(file_get_contents("php://input"));
 
-    $report->student_id = $data->student_id;
-    $report->reporter_id = $data->reporter_id;
-    $report->superior_id = $data->superior_id;
-    $report->course_code = $data->course_code;
-    $report->course_name = $data->course_name;
-    $report->exam_venue = $data->exam_venue;
-    $report->exam_date = $data->exam_date;
-    $report->exam_time = $data->exam_time;
-    $report->misconduct_time = $data->misconduct_time;
-    $report->misconduct_description = $data->misconduct_description;
-    $report->action_taken = $data->action_taken;
-    $report->witness1_name = $data->witness1_name;
-    $report->witness1_contact_no = $data->witness1_contact_no;
-    $report->witness1_email = $data->witness1_email;
-    $report->witness2_name = $data->witness2_name;
-    $report->witness2_contact_no = $data->witness2_contact_no;
-    $report->witness2_email = $data->witness2_email;
+    extract($data);
+    
+    $report->student_id = 'studentId';
+    $report->reporter_id = 'reporterId';
+    $report->superior_id = 'superiorId';
+    $report->course_code = 'courseCode';
+    $report->course_name = 'courseName';
+    $report->exam_venue = 'examVenue';
+    $report->exam_date = 'examDate';
+    $report->exam_time = 'examTime';
+    $report->misconduct_time = 'misconductTime';
+    $report->misconduct_description = 'misconductDescription';
+    $report->action_taken = 'actionTaken';
+    $report->witness1_name = 'witness1Name';
+    $report->witness1_contact_no = 'witness1ContactNo';
+    $report->witness1_email = 'witness1Email';
+    $report->witness2_name = 'witness2Name';
+    $report->witness2_contact_no = 'witness2ContactNo';
+    $report->witness2_email = 'witness2Email';
 
     if(!is_null($report->create())) {
         $report_id = $report->create();
