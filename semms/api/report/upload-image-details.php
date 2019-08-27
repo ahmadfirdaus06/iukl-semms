fe4af9a4535c8bfef3c097159444a06f4da311a0
ahmadfirdaus06 <ahmad0609firdaus@gmail.com>
2019-08-28 04:09:12 +0800

    configure api config

---
+++ semms/api/report/upload-image-details.php
@@ -0,0 +1,60 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../model/Attachment.php';
+
+    $database = new Database();
+    $db = $database->connect();
+
+    $attachment = new Attachment($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+    
+    $img_paths = $data->paths;
+    $attachment->report_id = $data->report_id;
+    $count = 0;
+    $total = count($img_paths);
+
+    foreach($img_paths as $path){
+        $attachment->path = $path;
+        if ($attachment->create()){
+            $count++;
+        }
+    }
+    if ($count == $total){
+        $conn = $db;
+        $report_id = $attachment->report_id;
+        $table = 'report';
+        $query = 'UPDATE ' . $this->table . '
+            SET 
+            is_valid = IFNULL(:is_valid, is_valid)
+            WHERE report_id = :report_id';
+            
+            $stmt = $conn->prepare($query);
+
+            $is_valid = htmlspecialchars(strip_tags('Yes')); 
+
+            $stmt->bindParam(':is_valid', $is_valid);
+            $stmt->bindParam(':report_id', $report_id);
+
+            if($stmt->execute()) {
+                echo json_encode(
+                    array('message' => 'Success')
+                );
+            }
+            else{
+                echo json_encode(
+                    array('message' => 'Failed')
+                );
+            }
+    }
+    else {
+        echo json_encode(
+            array('message' => 'Failed')
+        );
+    }
+?>
--- semms/api/report/upload-report-details.php
+++ semms/api/report/upload-report-details.php
@@ -13,34 +13,64 @@
     $report = new Report($db);
 
     $data = json_decode(file_get_contents("php://input"));
-
-    extract($data);
     
-    $report->student_id = 'studentId';
-    $report->reporter_id = 'reporterId';
-    $report->superior_id = 'superiorId';
-    $report->course_code = 'courseCode';
-    $report->course_name = 'courseName';
-    $report->exam_venue = 'examVenue';
-    $report->exam_date = 'examDate';
-    $report->exam_time = 'examTime';
-    $report->misconduct_time = 'misconductTime';
-    $report->misconduct_description = 'misconductDescription';
-    $report->action_taken = 'actionTaken';
-    $report->witness1_name = 'witness1Name';
-    $report->witness1_contact_no = 'witness1ContactNo';
-    $report->witness1_email = 'witness1Email';
-    $report->witness2_name = 'witness2Name';
-    $report->witness2_contact_no = 'witness2ContactNo';
-    $report->witness2_email = 'witness2Email';
+    $report->student_id = $data->student_id;
+    $report->reporter_id = $data->reporter_id;
+    $report->superior_id = $data->superior_id;
+    $report->course_code = $data->course_code;
+    $report->course_name = $data->course_name;
+    $report->exam_venue = $data->exam_venue;
+    $report->exam_date = $data->exam_date;
+    $report->exam_time = $data->exam_time;
+    $report->misconduct_time = $data->misconduct_time;
+    $report->misconduct_description = $data->misconduct_description;
+    $report->action_taken = $data->action_taken;
+    $report->witness1_name = $data->witness1_name;
+    $report->witness1_contact_no = $data->witness1_contact_no;
+    $report->witness1_email = $data->witness1_email;
+    $report->witness2_name = $data->witness2_name;
+    $report->witness2_contact_no = $data->witness2_contact_no;
+    $report->witness2_email = $data->witness2_email;
 
-    if(!is_null($report->create())) {
-        $report_id = $report->create();
-        echo json_encode(
+    $report_id = $report->create();
+
+    if(!is_null($report_id)) {
+        $misconduct_type = $data->misconduct_type;
+        $conn = $db;
+        $table = 'misconduct';
+        $count = 0;
+        $total = count($misconduct_type);
+
+        foreach($misconduct_type as $type){
+            $query = 'INSERT INTO ' . $table . ' SET 
+            type = :type,
+            report_id = :report_id';
+            
+            $stmt = $conn->prepare($query);
+
+            $type = htmlspecialchars(strip_tags($type));
+
+            $stmt->bindParam(':type', $type);
+            $stmt->bindParam(':report_id', $report_id);
+            if($stmt->execute()) {
+                $count++;
+            }
+            else{
+                printf("Error: %s.\n", $stmt->error);
+            }
+        }
+
+        if ($count == $total){
+            echo json_encode(
             array(
                 'message' => 'Success',
                 'report_id' => $report_id)
-        );
+            );
+        }else{
+            echo json_encode(
+                array('message' => 'Failed')
+            );
+        }
     } 
     else {
         echo json_encode(
