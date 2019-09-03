a51a415de3f09aee0519ad20f8198b9c58b773e6
ahmadfirdaus06 <ahmad0609firdaus@gmail.com>
2019-09-03 16:34:42 +0800

    Configure attachment api function

--- semms/model/Attachment.php
+++ semms/model/Attachment.php
@@ -23,40 +23,5 @@
 
             return $stmt;
         }
-
-        public function readByReportId(){
-            $query = 'SELECT * FROM ' . $this->table . ' WHERE report_id = ? LIMIT 0,1';
-
-            $stmt = $this->conn->prepare($query);
-
-            $stmt->bindParam(1, $this->report_id);
-
-            $stmt->execute();
-
-            return $stmt;
-        }
-
-        public function create(){
-            
-            $query = 'INSERT INTO ' . $this->table . ' SET 
-            path = :path,
-            report_id = :report_id';
-            
-            $stmt = $this->conn->prepare($query);
-
-            $this->path = htmlspecialchars(strip_tags($this->path));
-            $this->report_id = htmlspecialchars(strip_tags($this->report_id));
-
-            $stmt->bindParam(':path', $this->path);
-            $stmt->bindParam(':report_id', $this->report_id);
-
-            if($stmt->execute()) {
-                return true;
-            }
-
-            printf("Error: %s.\n", $stmt->error);
-
-            return false;
-        }
     }
 ?>
